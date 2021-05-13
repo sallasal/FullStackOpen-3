0d823137c83b419c34bfa523814ddf0ec76dbfec
sallasal <47078225+sallasal@users.noreply.github.com>
2021-05-13 09:58:59 +0300

    Add exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://puhelinluettelo:${password}@puhelinluettelo.zzewe.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length == 5) {
+    const person = new Person({
+        name: `${name}`,
+        number: `${number}`
+    })
+
+    person.save().then(response => {
+        mongoose.connection.close()
+    })
+} else if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    mongoose.connection.close()
+}
