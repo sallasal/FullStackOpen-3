2a1df7a862cf5f7f28a23ec3a47d211d622880b4
sallasal <47078225+sallasal@users.noreply.github.com>
2021-05-15 23:14:41 +0300

    Add exercise 3.14

--- index.js
+++ index.js
@@ -5,34 +5,12 @@ const cors = require('cors')
 const app = express()
 
 const Person = require('./models/person')
+const { response } = require('express')
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
-
-let connections = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Slalla Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abmarov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
 
 morgan.token('json', (req, res) => { 
     const stringres = JSON.stringify(req.body)
@@ -54,14 +32,9 @@ app.get('/api/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const contactInfo = connections.find(connection => connection.id === id)
-
-    if (contactInfo) {
-        res.json(contactInfo)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(req.params.id).then(note => {
+        res.json(note)
+    })
     
 })
 
@@ -72,31 +45,29 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-app.post('/api/persons/', (req, res) => {
-    const min = Math.ceil(0)
-    const max = Math.floor(87654)
-    const newId = Math.floor(Math.random() * (max-min) + min)
+app.post('/api/persons', (req, res) => {
+    const body = req.body
 
-    if (!req.body.name || !req.body.number) {
+    if (!body.name || !body.number) {
         return res.status(400).json({
             error: 'name or number is missing'
         })
     }
 
-    if (connections.some(c => c.name === req.body.name)) {
+    if (connections.some(c => c.name === body.name)) {
         return res.status(400).json({
             error: 'name must be unique'
         })
     }
+    
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-    const connection = {
-        id: newId,
-        name: req.body.name || false,
-        number: req.body.number || false
-    }
-
-    connections = connections.concat(connection)
-    res.json(connection)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT
