96a1aa333a8ffdacd7d69fff5571930e40472790
sallasal <47078225+sallasal@users.noreply.github.com>
2021-05-16 21:50:39 +0300

    Add exercise 3.19

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.put('/api/persons/:id', (req, res, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -89,13 +89,17 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => {
         res.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
     if (error.name === "CastError") {
         return res.status(400).send({ error: "Malformatted id" })
+    } else if (error.name === "ValidationError") {
+        return res.status(400).json({error: error.message})
     }
+
     next(error)
 }
 
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -12,11 +13,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        unique: true
+    },
     number: String
 })
 
-    personSchema.set('toJSON', {
+personSchema.plugin(uniqueValidator)
+
+personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
